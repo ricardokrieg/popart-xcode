87978581afacd446704e0a035392e2ceb8a38e46
Ricardo Franco <ricardo@MacBook-Pro-de-Ricardo.local>
2015-08-17 22:05:31 -0300

    up

---
+++ PopArt/Painting.swift
@@ -0,0 +1,20 @@
+//
+//  Painting.swift
+//  PopArt
+//
+//  Created by Ricardo Franco on 17/08/15.
+//  Copyright (c) 2015 Ricardo Franco. All rights reserved.
+//
+
+import Foundation
+
+struct Painting {
+    let image_url: String
+    let artist_name: String
+    let title: String
+    let characteristics: String
+    let dimensions_cm: String
+    let dimensions_in: String
+    let year: String
+    let code: String
+}
