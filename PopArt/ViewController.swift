2f879f547ad995ff8a57115a16b0e3a846d7ff47
Ricardo Franco <ricardo.krieg@gmail.com>
2015-09-02 02:06:07 -0300

    up

--- PopArt/ViewController.swift
+++ PopArt/ViewController.swift
@@ -209,7 +209,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
 //        self.view.layer.addSublayer(previewLayer)
         self.cameraView.layer.addSublayer(previewLayer)
-        previewLayer?.frame = self.cameraView.layer.frame
+        previewLayer?.frame = self.cameraView.bounds
 //        previewLayer?.frame = self.view.layer.frame
 //        previewLayer?.frame = self.view.bounds
 //        previewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
