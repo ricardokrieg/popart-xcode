bfaef6624fa931cda338b868a25b39e22b783a42
Ricardo Franco <ricardo.krieg@gmail.com>
2015-09-03 09:26:46 -0300

    up

---
+++ PopArt/MenuViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MenuViewController.swift
+//  PopArt
+//
+//  Created by Ricardo Franco on 02/09/15.
+//  Copyright (c) 2015 Ricardo Franco. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
