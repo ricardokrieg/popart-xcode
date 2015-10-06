2070f93a35aedcffa8194d9ed806f92091bbb066
Ricardo Franco <ricardo.krieg@gmail.com>
2015-10-05 22:43:35 -0300

    add Session model

---
+++ PopArt/Session.swift
@@ -0,0 +1,13 @@
+//
+//  Session.swift
+//  PopsArt
+//
+//  Created by Netronian Inc. on 05/10/15.
+//  Copyright © 2015 Netronian Inc. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Session: NSManagedObject {
+}
