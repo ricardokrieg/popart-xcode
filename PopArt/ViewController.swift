9ee751a2e6a3dc1b37763e75a9cea6764232f54f
Ricardo Franco <ricardo.krieg@gmail.com>
2015-12-01 11:55:55 -0300

    frame detect (wip)

--- PopArt/ViewController.swift
+++ PopArt/ViewController.swift
@@ -562,7 +562,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         let (detectedImage, croppedImage, detectMessage, top_left, top_right, bottom_left, bottom_right) = FrameDetectorView.detectUsingCIDetector(bufferImage)
         
         if detectedImage == nil {
-            print("fallback to GPUImage's HarrisCorner method")
+//            print("fallback to GPUImage's HarrisCorner method")
             
             server.frameDetector?.topLeft = nil
             server.frameDetector?.topRight = nil
@@ -594,7 +594,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         let colorSpace: CGColorSpaceRef = CGColorSpaceCreateDeviceRGB()!
         
         let bitsPerComponent: Int = 8
-        let newContext = CGBitmapContextCreate(nil, width, height, bitsPerComponent, 0, colorSpace, CGImageAlphaInfo.NoneSkipFirst.rawValue)
+        let newContext = CGBitmapContextCreate(nil, width, height, bitsPerComponent, bytesPerRow, colorSpace, CGImageAlphaInfo.NoneSkipFirst.rawValue)
         
         let imageRef: CGImageRef = CGBitmapContextCreateImage(newContext)!
         let resultImage = UIImage(CGImage: imageRef, scale: 1.0, orientation: UIImageOrientation.Right)
